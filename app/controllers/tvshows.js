351e10c0d96c9daa090ae0f870412a9bdd76e319
Nicolas Royo <nicolas@overtus.com>
2014-02-15 19:40:41 +0100

    Watch all bug

--- app/controllers/tvshows.js
+++ app/controllers/tvshows.js
@@ -196,8 +196,8 @@ exports.findByName = function(req, res){
             // query base
             TvShows.aggregate(
                 { $unwind: "$users" },
-                { $group: { _id: {c_id:"$_id", showid: "$showid", name: "$name", network: "$network"}, users_temp: {$addToSet: '$users'}, count: { $sum: 1 }}},
-                { $project: {_id : '$_id.c_id', showid: '$_id.showid', name: '$_id.name',network: '$_id.network', users: '$users_temp', num: '$count'}},
+                { $group: { _id: {c_id:"$_id", showid: "$showid", name: "$name", network: "$network", Episodelist: "$Episodelist"}, users_temp: {$addToSet: '$users'}, count: { $sum: 1 }}},
+                { $project: {_id : '$_id.c_id', showid: '$_id.showid', name: '$_id.name',network: '$_id.network', Episodelist: '$_id.Episodelist', users: '$users_temp', num: '$count'}},
                 { $limit: 50},
                 { $sort: {num: dir}}).exec(function(err, result){
                     //console.log(result);
